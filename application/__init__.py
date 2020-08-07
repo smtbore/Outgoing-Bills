45fd1e309d856b11c7b30c0080aaf6859ac5544d
Steven Bore <smtbore@gmail.com>
2020-08-07 11:28:16 +0100

    database uri and secret key configured

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from os import getenv
 
 app=Flask(__name__)
+
+#Database Setup
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SECRET_KEY'] = str(os.getenv('MY_SECRET_KEY'))
+db = SQLAlchemy(app)
 
 from application import routes
